afb630fbce0873cb07e6f74e6579d809535bb1e5
Priya Rathore <62906929+RATHORE7421@users.noreply.github.com>
2021-11-16 15:34:31 +0530

    Create Siding_Window.cpp

---
+++ Algorrithms/Siding_Window.cpp
@@ -0,0 +1,12 @@
+# Sliding Window:
+
+   int i=0 , j=0;
+    while(j<n){
+        // Calculations
+        if(j-i+1<B)
+        j++;
+        else if(j-i+1==B){
+          //ans
+          //Slide the window(i++ , j++)
+        }
+    }
